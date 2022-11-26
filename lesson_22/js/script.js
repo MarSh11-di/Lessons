52716cdca08c057f60705d1d9e87e6a9d28f52d7
MarSh11-di <112892986+MarSh11-di@users.noreply.github.com>
2022-11-26 20:45:24 +0100

    22

---
+++ lesson_22/js/script.js
@@ -0,0 +1,40 @@
+"use strict"
+// Iм'я змiнноi
+console.log('userEyeColor');
+
+let userName = 'Bасиль';
+let user;
+user = userName;
+console.log(user);
+
+let userAge = 25;
+console.log(userAge);
+console.log(typeof userAge);
+console.log(`Учим пучим`);
+console.log(typeof `Учим пучим`);
+
+let agentName = 'Ivan';
+let agentAge = 50;
+let agentInfo = `Iм'я: ${agentName}, вiк: ${agentAge}`;
+console.log(agentInfo);
+console.log(typeof agentInfo);
+
+let loto = false;
+if (loto) {
+   console.log(":)");
+} else {
+   console.log(":(");
+}
+
+let data = "38";
+let time = "56";
+console.log(data + time);
+console.log(typeof (data + time));
+console.log(data / time);
+console.log(typeof (data / time));
+console.log(data * time);
+console.log(typeof (data * time));
+
+let basa = "20" / "5";
+console.log(basa);
+console.log(typeof basa);
